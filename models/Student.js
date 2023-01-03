f6947e3e15e6c5747dcdbd8f177beb1a6edb7bbb
SinedAnoh <alocraxzz@gmail.com>
2023-01-03 21:09:04 +0200

    update student schema

--- models/Student.js
+++ models/Student.js
@@ -2,11 +2,27 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const StudentSchema = new Schema({
-    firstName: { type: "String", require: true },
-    secondName: { type: "String", require: true },
-    thirdName: { type: "String", require: true },
-    adress: { type: "String", require: true },
-    phone: { type: "String", require: true }
+    firstName: { type: "String", required: true },
+    secondName: { type: "String", required: true },
+    thirdName: { type: "String", required: true },
+    adress: { type: "String", required: true },
+    phone: { type: "String", required: true },
 });
+
+StudentSchema.statics.storeFromRequest = async function storeFromRequest(req) {
+    const student = new mongoose.model("Student", StudentSchema)(req.body);
+
+    await student.save();
+
+    return student;
+}
+
+StudentSchema.statics.updateFromRequest = async function updateFromRequest(req) {
+    const student = new mongoose.model("Student", StudentSchema)(req.body);
+
+    await this.model('Student').replaceOne({ _id: req.params.id }, student);
+
+    return student;
+}
 
 module.exports = mongoose.model("Student", StudentSchema);
